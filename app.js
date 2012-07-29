5eca8ff1225a4906a15ded1a283e8843193c6f7c
Jonathan Haber <HiJon89@gmail.com>
2012-07-29 13:19:34 -0400

    Remove express, stylus, and jade as the backend will just be returning JSON

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
+var http = require('http');
 
-/**
- * Module dependencies.
- */
+var port = process.env.PORT || 8000;
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
-
-var app = express();
-
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+var server = http.createServer(function (request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end("Hello World\n");
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+server.listen(port);
+console.log("Server running at http://localhost:" + port);
