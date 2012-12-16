feffa94f346530a9882565ae6beaba6acb4eb9c8
Jonathan Haber <HiJon89@gmail.com>
2012-12-15 20:41:20 -0500

    Switch to express instead of journey

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-var journey = require('journey');
+var express = require('express');
 
-var router = new(journey.Router);
+var router = express();
 require('filmsurf-routes').bind(router);
 
 var port = process.env.PORT || 8000;
-
-require('http').createServer(function (request, response) {
-	var body = "";
-
-    request.addListener('data', function (chunk) { body += chunk });
-    request.addListener('end', function () {
-        router.handle(request, body, function (result) {
-            response.writeHead(result.status, result.headers);
-            response.end(result.body);
-        });
-    });
-}).listen(port);
+router.listen(port);
 
 console.log("Server running at http://localhost:" + port);
