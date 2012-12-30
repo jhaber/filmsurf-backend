bdc88b13268eb8ad72140068aac03858f13fabe4
Jonathan Haber <HiJon89@gmail.com>
2012-12-30 17:36:07 -0500

    Some updates to Ebert

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer();
 
 var allowCrossDomain = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.header('origin'));
+    res.header('Access-Control-Allow-Origin', 'http://filmsurf.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
