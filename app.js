b177d27fd6a9a6fd31519fd1b095227bc7a31c84
Jonathan Haber <HiJon89@gmail.com>
2012-12-30 12:46:11 -0500

    Try to allow cross-site requests

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require('express');
 
-var router = express();
-require('filmsurf-routes').bind(router);
+var app = express.createServer();
+
+var allowCrossDomain = function(req, res, next) {
+    if (req.xhr) {
+        res.header("Access-Control-Allow-Origin", req.header('origin'));
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    }
+    next();
+};
+
+app.configure(function () {
+    app.use(allowCrossDomain);
+    app.use(app.router);
+});
+
+require('filmsurf-routes').bind(app);
 
 var port = process.env.PORT || 8000;
-router.listen(port);
+app.listen(port);
 
 console.log("Server running at http://localhost:" + port);
